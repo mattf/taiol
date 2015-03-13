0c52ca99d9da37d92522f946112f65ea4e8d7f07
Matthew Farrellee <matt@redhat.com>
2015-03-13 07:53:51 -0400

    add datafile and address params to play.py

--- play.py
+++ play.py
@@ -1,19 +1,30 @@
 #!/bin/python
 
 import json
+import sys
+import time
 
 from proton import *
+
+datafile = sys.argv[1]
+address = sys.argv[2]
 
 mng = Messenger()
 mng.start()
 
-for line in open('data.json'):
-    event = json.loads(line)
-    msg = Message()
-    msg.address = 'amqp://1.2.3.4/donotuse'
-    msg.properties = event
-    mng.put(msg)
-    mng.send()
-    print event
+lc = 0
+ec = 0
+while True:
+    print "loop count:", lc, "event count:", ec
+    for line in open(datafile):
+        event = json.loads(line)
+        msg = Message()
+        msg.address = address
+        msg.properties = event
+        mng.put(msg)
+        mng.send()
+        time.sleep(0.01)
+        ec += 1
+    lc += 1
 
 mng.stop()
