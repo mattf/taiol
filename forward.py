6e33a71c573330018cb00ac0192471cd86b6c54f
Matthew Farrellee <matt@redhat.com>
2015-04-15 12:42:08 -0400

    add --port option

--- forward.py
+++ forward.py
@@ -9,6 +9,8 @@ from proton import *
 parser = optparse.OptionParser(
     "usage: %prog <address>",
     description="Forward AMQP 1.0 message properties to a listener as JSON")
+parser.add_option("-p", "--port", default=1984,
+                  help="Port to listen on")
 opts, args = parser.parse_args()
 
 mng = Messenger()
@@ -22,7 +24,7 @@ for a in args:
 msg = Message()
 sock = socket.socket()
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind(('0.0.0.0', 1984))
+sock.bind(('0.0.0.0', int(opts.port)))
 print 'listening at:', sock.getsockname()
 sock.listen(1)
 while True:
